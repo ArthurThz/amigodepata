fdae845e96e8a40f4c07c7c9d2f18a2b5ea94e9a
unknown <arthur.theodoro.sva@hotmail.com>
2024-10-16 17:14:38 -0300

    feat: created our team component mobile view

---
+++ app/Components/AboutUs/ourTeam.tsx
@@ -0,0 +1,44 @@
+import ArthurProfilePicutre from "../../public/arthur-profile-picture.jpeg";
+import JujuProfilePicutre from "../../public/juju-profile-picture.jpeg";
+import FinoProfilePicture from "../../public/fino-profile-picture.jpeg";
+import LuigiProfilePicutre from "../../public/luigi-profile-picture.jpeg";
+import GuilhermeProfilePicutre from "../../public/guilherme-profile-picture.jpeg";
+import RafaProfilePicutre from "../../public/rafa-profile-picture.jpg";
+import ProfilePicture from "./profilePicture";
+
+const OurTeam = () => {
+  return (
+    <div className="w-full min-h-screen flex flex-col items-center px-10 py-16 gap-10 bg-azul-50 lg:justify-center">
+      <h1 className="font-poetsenOne text-3xl text-azul-800 lg:text-6xl">
+        Quem Somos?
+      </h1>
+      <div className="w-full h-auto grid grid-rows-4 grid-cols-2 place-items-center  gap-12 lg:auto-rows-auto lg:grid-cols-3">
+        <ProfilePicture image={JujuProfilePicutre} nome="Julia Neves" />
+        <ProfilePicture image={ArthurProfilePicutre} nome="Arthur Theodoro" />
+        <ProfilePicture image={FinoProfilePicture} nome="Raphael Clivatti" />
+        <ProfilePicture image={RafaProfilePicutre} nome="Rafaella Seivane" />
+        <ProfilePicture image={LuigiProfilePicutre} nome="Luigi Boscariol" />
+        <ProfilePicture
+          image={GuilhermeProfilePicutre}
+          nome="Guilherme Gomes"
+        />
+        <ProfilePicture
+          image={GuilhermeProfilePicutre}
+          nome="Guilherme Gomes"
+        />
+      </div>
+      <h2 className="font-poetsenOne text-azul-800">
+        Somos um grupo de estudantes de Sistemas de Informação que, durante a
+        proposta de um trabalho nos solidarizamos com as pessoas que possuem
+        algum tipo de deficiência.
+      </h2>
+      <h2 className="font-poetsenOne text-azul-800">
+        Durante uma pesquisa, notamos que os animais podem fornecer uma melhora
+        significativa para estas pessoas.Portanto, decidimos unir os dois e
+        tentar proporcionar uma vida melhor para as duas partes.
+      </h2>
+    </div>
+  );
+};
+
+export default OurTeam;
---
+++ app/Components/AboutUs/profilePicture.tsx
@@ -0,0 +1,25 @@
+import Image, { StaticImageData } from "next/image";
+
+type ProfilePictureProps = {
+  image: StaticImageData;
+  nome: string;
+};
+
+const ProfilePicture = ({ image, nome }: ProfilePictureProps) => {
+  return (
+    <div className="min-w-[40%] w-full  h-full flex flex-col items-center justify-center lg:w-auto ring-2">
+      <h3 className="font-poetsenOne text-md text-azul-800 mb-2 ">{nome}</h3>
+      <div className=" w-[130px] h-[130px] lg:w-[210px] lg:h-[210px] flex flex-col items-center  gap-5 border-2 p-[2px] border-azul-900 rounded-full shadow-md shadow-azul-400 lg:border-4 lg:p-[4px]">
+        <Image
+          className="rounded-full w-[150px] h-full lg:w-full lg:h-full"
+          src={image}
+          width={1080}
+          height={1080}
+          alt={`${image} picture`}
+        />
+      </div>
+    </div>
+  );
+};
+
+export default ProfilePicture;
