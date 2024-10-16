2d29d1d890e4efa6c0da36996c81e6fe4e5130a3
unknown <arthur.theodoro.sva@hotmail.com>
2024-10-16 14:43:00 -0300

    feat: responsive view for ourPets and ourHorses components

--- app/Components/MeetOutPets/Horses/index.tsx
+++ app/Components/MeetOutPets/Horses/index.tsx
@@ -3,25 +3,25 @@ import BannerCavalo from "../../../public/ourpets-equoterapia.jpg";
 
 const Horses = () => {
   return (
-    <div className="w-full h-auto flex flex-col items-center bg-laranja-200">
-      <div className="w-auto h-auto flex flex-col items-center gap-5 py-10 px-5">
-        <h1 className="font-poetsenOne text-2xl text-azul-800">
+    <div className="w-full h-auto flex flex-col items-center bg-laranja-200 lg:flex-row lg:h-[750px]">
+      <div className="w-auto h-auto flex flex-col items-center gap-5 py-10 px-5 lg:w-[50%] lg:h-full lg:justify-center lg:gap-10">
+        <h1 className="font-poetsenOne text-2xl text-azul-800 lg:text-5xl">
           Nossos Cavalos
         </h1>
-        <p className="font-roboto text-azul-900 font-normal text-md">
+        <p className="font-roboto text-azul-900 font-normal text-md lg:w-[60%] text-xl">
           São treinados para oferecer a melhor qualidade em nossas terapias,
           nossos cuidadores garantem o melhor tratamento possível para nossos
           amigos de patas.
         </p>
-        <button className="px-10 py-2 border-2 border-azul-900 bg-azul-900 text-white font-roboto rounded-lg shadow-lg shadow-laranja-300 hover:bg-laranja-300/50 hover:text-azul-900 hover:font-bold hover:shadow-azul-800 transition-all ease">
+        <button className="px-10 py-2 border-2 border-azul-900 bg-azul-900 font-bold text-white font-roboto rounded-lg shadow-lg shadow-laranja-300 hover:bg-laranja-300/50 hover:text-azul-900 hover:shadow-azul-800 transition-all ease">
           Conheça nossos cavalos
         </button>
       </div>
       <Image
-        className="w-full h-[260px]"
+        className="w-full h-[260px] lg:w-[60%] lg:h-full"
         src={BannerCavalo}
-        width={500}
-        height={500}
+        width={1920}
+        height={1080}
         alt="imagem cavalo e menina"
       />
     </div>
--- app/Components/MeetOutPets/Pets/index.tsx
+++ app/Components/MeetOutPets/Pets/index.tsx
@@ -3,23 +3,25 @@ import BannerPets from "../../../public/oupets-suporte-emocional.jpg";
 
 const Pets = () => {
   return (
-    <div className="w-full h-auto flex flex-col items-center bg-azul-800">
-      <div className="w-auto h-auto flex flex-col items-center gap-5 py-10 px-5">
-        <h1 className="font-poetsenOne text-2xl text-white">Nossos Pets</h1>
-        <p className="font-roboto text-white font-normal text-md">
+    <div className="w-full h-auto flex flex-col items-center bg-azul-800 lg:flex-row-reverse lg:h-full">
+      <div className="w-auto h-auto flex flex-col items-center gap-5 py-10 px-5 lg:w-[50%] lg:h-full lg:justify-center lg:gap-10">
+        <h1 className="font-poetsenOne text-2xl text-white lg:text-5xl">
+          Nossos Pets
+        </h1>
+        <p className="font-roboto text-white font-normal text-md lg:w-[60%] text-xl">
           São resgatados de situação de rua ou abandono, fazemos a conexão entre
           pessoas que precisam de um amigo para terem uma vida melhor e um pet
           que precisa de um lar.
         </p>
-        <button className="px-10 py-2 border-2 border-laranja-200 bg-laranja-200 text-azul-900 font-bold font-roboto rounded-lg shadow-lg shadow-azul-800 hover:bg-azul-800 hover:text-laranja-200 hover:shadow-azul-100 transition-all ease">
+        <button className="px-10 py-2 border-2 border-laranja-200 bg-laranja-200 text-azul-900 font-bold font-roboto rounded-lg shadow-md shadow-azul-800 hover:bg-azul-800 hover:text-laranja-200 hover:shadow-laranja-300 transition-all ease">
           Conheça nossos pets
         </button>
       </div>
       <Image
-        className="w-full h-[260px]"
+        className="w-full h-[260px] lg:w-[60%] lg:h-full"
         src={BannerPets}
-        width={500}
-        height={500}
+        width={1920}
+        height={1080}
         alt="imagem cavalo e menina"
       />
     </div>
