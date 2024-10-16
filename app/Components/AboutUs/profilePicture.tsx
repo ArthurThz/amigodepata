a7f25f1ae6e807f5bc1c0917e20fce4235608ddc
unknown <arthur.theodoro.sva@hotmail.com>
2024-10-16 17:15:19 -0300

    feat: removed ring from profile pic

--- app/Components/AboutUs/profilePicture.tsx
+++ app/Components/AboutUs/profilePicture.tsx
@@ -7,7 +7,7 @@ type ProfilePictureProps = {
 
 const ProfilePicture = ({ image, nome }: ProfilePictureProps) => {
   return (
-    <div className="min-w-[40%] w-full  h-full flex flex-col items-center justify-center lg:w-auto ring-2">
+    <div className="min-w-[40%] w-full  h-full flex flex-col items-center justify-center lg:w-auto">
       <h3 className="font-poetsenOne text-md text-azul-800 mb-2 ">{nome}</h3>
       <div className=" w-[130px] h-[130px] lg:w-[210px] lg:h-[210px] flex flex-col items-center  gap-5 border-2 p-[2px] border-azul-900 rounded-full shadow-md shadow-azul-400 lg:border-4 lg:p-[4px]">
         <Image
