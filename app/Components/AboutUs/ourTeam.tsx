2249f1f93e44e0fe598bb3367b9e1eb3b18c57a0
unknown <arthur.theodoro.sva@hotmail.com>
2024-10-18 14:00:51 -0300

    feat: desktop view for footer component, increased font-size in our team component text

--- app/Components/AboutUs/ourTeam.tsx
+++ app/Components/AboutUs/ourTeam.tsx
@@ -8,16 +8,16 @@ import ProfilePicture from "./profilePicture";
 
 const OurTeam = () => {
   return (
-    <div className="w-full min-h-screen flex flex-col items-center px-10 py-16 gap-10 bg-azul-50 lg:justify-center">
+    <div className="w-full min-h-screen flex flex-col items-center px-10 py-16 gap-10 lg:gap-14 bg-azul-50 lg:justify-center">
       <h1 className="font-poetsenOne text-3xl text-azul-800 lg:text-6xl">
         Quem Somos?
       </h1>
-      <h2 className="font-poetsenOne text-azul-800">
+      <h2 className="font-poetsenOne text-azul-800 lg:w-[40%] lg:text-xl">
         Somos um grupo de estudantes de Sistemas de Informação que, durante a
         proposta de um trabalho nos solidarizamos com as pessoas que possuem
         algum tipo de deficiência.
       </h2>
-      <h2 className="font-poetsenOne text-azul-800">
+      <h2 className="font-poetsenOne text-azul-800 lg:w-[40%] lg:text-xl">
         Durante uma pesquisa, notamos que os animais podem fornecer uma melhora
         significativa para estas pessoas.Portanto, decidimos unir os dois e
         tentar proporcionar uma vida melhor para as duas partes.
--- app/Components/Footer/index.tsx
+++ app/Components/Footer/index.tsx
@@ -7,46 +7,46 @@ import { RiInstagramFill } from "react-icons/ri";
 const Footer = () => {
   return (
     <footer className="w-full py-10 px-10 flex flex-col items-center justify-center bg-laranja-200">
-      <div className="w-auto flex-flex-col items-start  lg:flex-row lg:justify-between ">
+      <div className="w-[80%]  flex flex-col items-start  lg:flex-row lg:justify-between ">
         <div className="w-auto flex flex-col items-start">
           <h3 className="text-azul-800 font-poetsenOne text-2xl flex items-center gap-2 mb-10">
             Amigos de pata <IoPaw />
           </h3>
           <nav className="flex flex-col items-start gap-2 text-azul-800 font-roboto font-medium">
             <Link
-              className="text-lg text-azul-900 font-roboto font-medium hover:text-laranja-600 transition-all"
+              className="text-lg text-azul-900 font-roboto font-medium hover:text-laranja-900 hover:font-bold transition-all"
               href=""
             >
               Home
             </Link>
             <Link
-              className="text-lg text-azul-900 font-roboto font-medium hover:text-laranja-600 transition-all"
+              className="text-lg text-azul-900 font-roboto font-medium hover:text-laranja-900 hover:font-bold transition-all"
               href=""
             >
               Nossos Pets
             </Link>
             <Link
-              className="text-lg text-azul-900 font-roboto font-medium hover:text-laranja-600 transition-all"
+              className="text-lg text-azul-900 font-roboto font-medium hover:text-laranja-900 hover:font-bold transition-all"
               href=""
             >
               Sobre nós
             </Link>
             <Link
-              className="text-lg text-azul-900 font-roboto font-medium hover:text-laranja-600 transition-all"
+              className="text-lg text-azul-900 font-roboto font-medium hover:text-laranja-900 hover:font-bold transition-all"
               href=""
             >
               Cadastro
             </Link>
 
             <div className="w-full flex gap-2 items-center mt-2">
-              <RiInstagramFill className="text-azul-800 text-3xl" />
-              <FaFacebookSquare className="text-azul-800 text-3xl" />
-              <AiFillTikTok className="text-azul-800 text-3xl" />
+              <RiInstagramFill className="text-azul-800 text-3xl hover:text-laranja-900 transition-all ease hover:cursor-pointer hover:scale-[1.2]" />
+              <FaFacebookSquare className="text-azul-800 text-3xl hover:text-laranja-900 transition-all ease hover:cursor-pointer hover:scale-[1.2]" />
+              <AiFillTikTok className="text-azul-800 text-3xl hover:text-laranja-900 transition-all ease hover:cursor-pointer hover:scale-[1.2]" />
             </div>
           </nav>
         </div>
 
-        <h2 className="font-poetsenOne text-2xl text-azul-800 mt-10">
+        <h2 className="font-poetsenOne text-2xl text-azul-800 mt-10 lg:w-[25%]">
           "O amor incondicional de um animal é uma terapia silenciosa, trazendo
           paz e alívio para quem enfrenta os desafios do dia a dia."
         </h2>
