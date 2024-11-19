bd3423df9d784262aa64bf23b3660382ee5f9703
unknown <arthur.theodoro.sva@hotmail.com>
2024-11-19 15:42:08 -0300

    feat: add error component

--- app/Components/Error/index.tsx
+++ app/Components/Error/index.tsx
@@ -1,9 +1,15 @@
+import { GiSittingDog } from "react-icons/gi";
+
 const ErrorMessage = () => {
   return (
-    <div className="w-full h-screen bg-red-500 flex items-center justify-center">
-      <h1 className="text-5xl">
-        Parece que algo deu errado, tente novamente mais tarde!
-      </h1>
+    <div className="w-full h-screen bg-white flex flex-col items-center justify-center gap-5 ">
+      <div className="flex items-center justify-center w-[40%] gap-5">
+        <GiSittingDog className="text-[500px]" />
+
+        <h1 className="text-[50px] font-poetsenOne text-red-600">
+          Parece que algo deu errado, tente novamente mais tarde!
+        </h1>
+      </div>
     </div>
   );
 };
