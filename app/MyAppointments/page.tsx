4f62b1857bc2f9d80228f8371091e2e7f996c430
ArthurThz <theodorodev@outlook.com>
2024-11-20 12:17:12 -0300

    feat: add my appointments page

---
+++ app/MyAppointments/page.tsx
@@ -0,0 +1,9 @@
+const MyAppointments = () => {
+  return (
+    <div className="w-full h-full grid grid-cols-2">
+      <div className="w-[30%] border border-azul-900 min-h-[100px] h-auto px-4 py-2"></div>
+    </div>
+  );
+};
+
+export default MyAppointments;
