637d6322ba5d883ae5565162a125b83ec5e657ca
unknown <arthur.theodoro.sva@hotmail.com>
2024-11-22 14:03:32 -0300

    feat: add filter to suport animals card container

--- app/Components/our-pets/SupportAnimals/index.tsx
+++ app/Components/our-pets/SupportAnimals/index.tsx
@@ -3,19 +3,41 @@ import CardContainer from "../Card/cardsContainer";
 import Loader from "../../Loader";
 
 const SupportAnimals = () => {
-  const { Animals, isLoading } = useSupportAnimals();
+  const { FilteredAnimals, isLoading, filterAnimals, getAllAnimals } =
+    useSupportAnimals();
+
   return (
-    <div className="w-full py-10 h-auto bg-azul-200 flex flex-col items-center gap-16 lg:py-20">
+    <div className="w-full py-10 h-auto bg-azul-200 flex flex-col items-center justify-center gap-16 lg:py-20">
       <h1 className="font-poetsenOne text-azul-900 text-2xl text-center px-4 lg:px-0 lg:text-5xl lg:text-start lg:mb-10">
         Amigos de pata prontos para te dar apoio
       </h1>
+      <div className="flex items-center justify-start gap-5 ">
+        <button
+          onClick={() => getAllAnimals()}
+          className="text-sm  font-roboto px-6 py-2 rounded-full bg-white border-2 border-azul-900 hover:opacity-80"
+        >
+          Todos
+        </button>
+        <button
+          onClick={() => filterAnimals({ animalType: "cachorro" })}
+          className="text-sm  font-roboto px-6 py-2 rounded-full bg-white border-2 border-azul-900 hover:opacity-80"
+        >
+          Cachorro
+        </button>
+        <button
+          onClick={() => filterAnimals({ animalType: "gato" })}
+          className="text-sm  font-roboto px-6 py-2 rounded-full bg-white border-2 border-azul-900 hover:opacity-80"
+        >
+          Gato
+        </button>
+      </div>
       {isLoading ? (
         <>
           <Loader image="/dogloader.gif" />
         </>
       ) : (
         <CardContainer
-          data={Animals}
+          data={FilteredAnimals}
           CardButtonClass="bg-azul-900 text-white  hover:text-azul-900 hover:border-azul-900  hover:bg-azul-300"
           CardClass="bg-white border-white text-azul-900 shadow-azul-900 hover:border-azul-900 hover:shadow-azul-900"
         />
--- app/Hooks/useSupportAnimals.ts
+++ app/Hooks/useSupportAnimals.ts
@@ -1,22 +1,47 @@
 import { useEffect, useState } from "react";
 import { API_ROUTE } from "../Services/api";
 import { AnimalProps } from "../Types/Animals";
+import { animate } from "motion";
 
 
 export const useSupportAnimals =  () =>{
     const [Animals, setAnimals] = useState<AnimalProps[]>([]);
+    const [FilteredAnimals, setFilteredAnimals] = useState<AnimalProps[]>([]);
     const [isLoading, setIsLoading] = useState(false);
+
+    const filterAnimals = ({animalType} : {animalType : "cachorro" | "gato" }) =>{
+
+        if(animalType === "cachorro"){
+            let filteredAnimals = Animals.filter((animal) => animal.tipoanimal === animalType)
+            setFilteredAnimals(filteredAnimals)
+        }
+        if(animalType === "gato"){
+            let filteredAnimals = Animals.filter((animal) => animal.tipoanimal === animalType)
+            setFilteredAnimals(filteredAnimals)
+        }
+
+       
+    }
+
+    const getAllAnimals = () =>{
+        setFilteredAnimals(Animals)
+    }
   
     const fetchAnimals = async() =>{
         setIsLoading(true)
         const response = await API_ROUTE.get("/SupportAnimals")
         setAnimals(response.data.content)
+        setFilteredAnimals(response.data.content)
         setIsLoading(false)
     }   
-
+   
     useEffect(() => {
-        fetchAnimals()
-    }, []);
+        if(Animals.length === 0){
+            console.log("fetched Data")
 
-    return {Animals, isLoading}
+            fetchAnimals()
+        }
+    }, [filterAnimals]);
+
+    return {Animals, isLoading, filterAnimals, FilteredAnimals, getAllAnimals}
 }
